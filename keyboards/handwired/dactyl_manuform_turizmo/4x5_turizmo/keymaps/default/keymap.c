9338a8987afbcb972cd8d4f0b71b7e1d08059327
Turizmo <stianfoss.90+github@gmail.com>
2025-05-16 19:51:18 +0200

    First map

--- keyboards/handwired/dactyl_manuform_turizmo/4x5_turizmo/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_turizmo/4x5_turizmo/keymaps/default/keymap.c
@@ -67,93 +67,49 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base (qwerty)
-     * ,----------------------------------,                             ,----------------------------------,
-     * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |
-     * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |
-     * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
-     * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        |  [   |   ]  |                                                         |   -  |   =  |
-     *        '------+------'-------------'                             '-------------'------+------'
-     *                      | ESC  |  BS  |                             | SPACE|ENTER |
-     *                      |  +   |   +  |                             |  +   |  +   |
-     *                      | SHIFT| CTRL |                             | ALT  |SHIFT |
-     *                      '------+------'                             '------+------'
-     *                                    '------+------' '------+------'
-     *                                    | TAB  | HOME | | END  | DEL  |
-     *                                    '------+------' '------+------'
-     *                                    | Raise|  ~   | | GUI  | Lower|
-     *                                    '------+------' '------+------'
-     */
-    [_BASE] = LAYOUT(
-        KC_Q,       KC_W,       KC_E,      KC_R,       KC_T,                       KC_Y,        KC_U,       KC_I,       KC_O,       KC_P,
-        KC_A,       KC_S,       KC_D,      KC_F,       KC_G,                       KC_H,        KC_J,       KC_K,       KC_L,       KC_N,
-        KC_Z,       KC_X,       KC_C,      KC_V,       KC_B,                       NO_AE,       KC_M,       KC_COMM,    KC_DOT,     KC_QUOT,
-                    KC_LBRC,    KC_RBRC,                                                                    NO_OSTR,    NO_ARNG,
-                                                LT(1,KC_ENT),KC_SPC,        KC_BSPC,    LT(2, KC_ESC),
-                                                KC_TAB,     KC_HOME,        KC_END,     KC_DEL,
-                                                RAISE,      QK_BOOT,        KC_LGUI,    LOWER
+
+    [0] = LAYOUT(  //   QWERY layer
+
+        KC_Q,       KC_W,       KC_E,      KC_R,       KC_T,                    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
+        KC_A,       KC_S,       KC_D,      KC_F,       KC_G,                    GUI_H,      GUI_J,      GUI_K,      GUI_L,      KC_N,
+        KC_Z,       KC_X,       KC_C,      KC_V,       KC_B,                    NO_AE,      KC_M,       KC_COMM,    KC_DOT,     KC_QUOT,
+                    NO_QUES,    NO_EXLM,                                                                NO_OSTR,    NO_ARNG,
+                                                KC_SPC,LT(1,KC_ENT),        LT(2, KC_ESC),KC_BSPC,
+                                                KC_LCTL,    KC_LGUI,        KC_LSFT,    KC_LALT,
+                                                NO_QUOT,    NO_DQUO,        NO_AT,      KC_RCTL
     ),
 
-    /* Raise
-     * ,----------------------------------,                             ,----------------------------------,
-     * |      |      |  mup |      |      |                             | VOL+ |      |  up  |      | PgUp |
-     * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |      | mleft| mdown|mright|      |                             | MUTE | left | down |right | PgDn |
-     * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |      |      |      |      |      |                             | VOL- |  /   |  \   |  ?   |  |   |
-     * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        |      |      |                                                         | mbtn |mbtn2 |
-     *        '------+------'-------------'                             '-------------'------+------'
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      '------+------'                             '------+------'
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     */
-    [_RAISE] = LAYOUT(
-        _______, _______, KC_MU,   _______, _______,                                      KC_VOLU, _______, KC_UP,   _______, KC_PGUP,
-        _______, KC_ML,   KC_MD,   KC_MR,   _______,                                      KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-        _______, _______, _______, _______, _______,                                      KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
-                 _______, _______,                                                                          KC_MB1,  KC_MB2,
-                                   _______, _______,                                      _______, _______,
-                                                     _______, _______,  _______, _______,
-                                                     _______, _______,  _______, _______
+
+    [1] = LAYOUT(  //   Navigation layer
+
+        KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_INS,                 R_WH_L,     R_WH_D,     R_WH_U,     R_WH_R,     KC_DEL,
+        R_SET_1,    R_SET_2,    R_SET_3,    R_SET_4,    MS_BTN4,                R_LEFT,     R_DOWN,     R_UP,       R_LEFT,     KC_TAB,
+        R_TL_ACL,   MS_BTN3,    MS_BTN2,    MS_BTN1,    MS_BTN5,                R_MS_L,     R_MS_D,     R_MS_U,     R_MS_R,     KC_APP,
+                    XXXXXXX,    XXXXXXX,                                                                XXXXXXX,    XXXXXXX,
+                                                _______,    _______,        MO(3),      _______,
+                                                _______,    _______,        _______,    _______,
+                                                XXXXXXX,    XXXXXXX,        XXXXXXX,    _______
     ),
 
-    /* Lower
-     * ,----------------------------------,                             ,----------------------------------,
-     * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
-     * |------+------+------+------+------|                             |-------------+------+------+------|
-     * |  1   |  2   |  3   |  4   |  5   |                             |  6   |  7   |  8   |  9   |  0   |
-     * |------+------+------+------+------|                             |------|------+------+------+------|
-     * |  !   |  @   |  #   |  $   |  %   |                             |  ^   |  &   |  *   |  (   |  )   |
-     * |------+------+------+-------------,                             ,-------------+------+------+------,
-     *        | F11  | F12  |                                                         |  -   |  =   |
-     *        '------+------'-------------'                             '-------------'------+------'
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      |      |      |                             |      |      |
-     *                      '------+------'                             '------+------'
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     *                                    |      |      | |      |      |
-     *                                    '------+------' '------+------'
-     */
-    [_LOWER] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                 KC_F11,  KC_F12,                                                                           _______, _______,
-                                   _______, _______,                                      _______, _______,
-                                                     _______, _______,  _______, _______,
-                                                     _______, _______,  _______, _______
+    [2] = LAYOUT(  //   Number and symbols layer
+                 //
+        NO_LBRC,    NO_LCBR,    NO_LPRN,    NO_SLSH,    NO_LABK,                NO_RABK,    NO_BSLS,    NO_RPRN,    NO_RCBR,    NO_RBRC,
+        KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
+        KC_PERC,    KC_EQL,     NO_ASTR,    NO_PLUS,    NO_HASH,                NO_AMPR,    NO_MINS,    KC_COMM,    KC_DOT,     KC_UNDS,
+                    NO_DLR,     NO_PIPE,                                                                NO_CIRC,    XXXXXXX,
+                                                _______,    MO(3),          _______,    _______,
+                                                _______,    _______,        _______,    _______,
+                                                XXXXXXX,    XXXXXXX,        XXXXXXX,    _______
+    ),
+
+    [3] = LAYOUT(  //   Function layer
+
+        QK_BOOT,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                KC_MPREV,   KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_MPLY,
+        KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                  KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,
+        KC_F11,     KC_F12,     XXXXXXX,    XXXXXXX,    XXXXXXX,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+                    XXXXXXX,    XXXXXXX,                                                                XXXXXXX,    XXXXXXX,
+                                                _______,    _______,        _______,    _______,
+                                                _______,    _______,        _______,    _______,
+                                                XXXXXXX,    XXXXXXX,        XXXXXXX,    _______
     )
-};
+}
